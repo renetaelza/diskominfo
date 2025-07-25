0fea8741ff711a761dfbe6108e2b6bc9745d7475
Gilang Guna Pangestu <gilangpangestu515@gmail.com>
2025-07-25 14:27:35 +0700

    struktur awal dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Controllers\Admin\DashboardController;
+
+
 
 Route::get('/', function () {
     return view('landing');
+});
+
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'login'])->name('admin.login.submit');
+Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
+Route::middleware(['auth:admin', IsAdmin::class])->group(function () {
+    Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 });
 
 Route::get('/berita', [BeritaController::class, 'index'])->name('berita.index');
