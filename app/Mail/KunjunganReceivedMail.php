e3ceb5ee530e0b2b2865735e80af26db0a99f5f0
NailPrihartono <na'il.ghani@mhs.itenas.ac.id>
2025-09-01 17:17:09 +0700

    Menghilangkan ShouldQueue untuk mail

--- app/Mail/KunjunganReceivedMail.php
+++ app/Mail/KunjunganReceivedMail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class KunjunganReceivedMail extends Mailable implements ShouldQueue
+class KunjunganReceivedMail extends Mailable
 {
     use Queueable, SerializesModels;
 
