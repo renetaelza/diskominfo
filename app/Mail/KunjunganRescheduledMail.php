e5dfa17911dee6bc7af7d714c5d722a201aaa488
NailPrihartono <na'il.ghani@mhs.itenas.ac.id>
2025-09-01 16:56:11 +0700

    Menambahkan Implements ShouldQueue

--- app/Mail/KunjunganRescheduledMail.php
+++ app/Mail/KunjunganRescheduledMail.php
@@ -4,12 +4,13 @@ namespace App\Mail;
 
 use App\Models\Kunjungan;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class KunjunganRescheduledMail extends Mailable
+class KunjunganRescheduledMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
